16e44b8bdec0853a08554b15ba5a89b131ba867c
Andyyy7666 <86536434+Andyyy7666@users.noreply.github.com>
2021-12-18 21:53:19 +0100

    Update config.lua
    Added multiple default spawn points to config

--- config.lua
+++ config.lua
@@ -1,7 +1,10 @@
 config = {}
 
 config.defaultPed = "a_m_m_acult_01" -- default ped name 
-config.spawnPoint = vector3(687.03, 577.43, 130.46) -- default spawn point
+config.spawnPoint = {
+    {vector = vector3(687.03, 577.43, 130.46)},
+    {vector = vector3(687.03, 577.43, 130.46)}
+} -- default spawn points
 config.maxDistance = 30 -- max distance before it sends you back
 
 config.spawnBackMessage = "Change your ped bro -_-"
