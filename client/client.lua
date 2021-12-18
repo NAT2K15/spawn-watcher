32e00e9b4970dcb8c0c9eb3e6d16537346aa8ef1
Andyyy7666 <86536434+Andyyy7666@users.noreply.github.com>
2021-12-18 21:58:56 +0100

    Update client.lua
    Added support for multiple default spawn points.

--- client/client.lua
+++ client/client.lua
@@ -2,13 +2,14 @@ Citizen.CreateThread(function()
     while true do
         Citizen.Wait(2000)
         local ped = PlayerPedId()
-
-        if GetDistanceBetweenCoords(config.spawnPoint, GetEntityCoords(ped)) > config.maxDistance then
+            
+        spawnPoint = config.spawnPoint[math.random(1, #config.spawnPoint)].vector
+        if GetDistanceBetweenCoords(spawnPoint, GetEntityCoords(ped)) > config.maxDistance then
             local model = GetEntityModel(ped)
             local hashInfo = FoundObjects[model]
 
             if hashInfo and model == GetHashKey(config.defaultPed) then
-                SetEntityCoords(ped, config.spawnPoint)
+                SetEntityCoords(ped, spawnPoint)
                 TriggerEvent('chat:addMessage', {
                     color = { 255, 0, 0},
                     multiline = true,
