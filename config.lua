4efff61bbac665d5ba8427fa6b2a5c86580a685f
Marshular <marshy0909@gmail.com>
2024-01-30 20:51:49 -0800

    refactor

--- config.lua
+++ config.lua
@@ -1,7 +1,14 @@
 config = {}
 
-config.defaultPed = "a_m_m_acult_01" -- default ped name 
-config.spawnPoint = vector3(687.03, 577.43, 130.46) -- default spawn point
+config.defaultPed = `a_m_m_acult_01` -- default ped name 
+
+-- List of spawn points
+config.spawnPoints = {
+    vector3(2727.41, 1363.52, 24.52),
+    vector3(700.0, 600.0, 130.0), -- Add more spawn points as needed
+    -- Add more spawn points as needed
+}
+
 config.maxDistance = 30 -- max distance before it sends you back
 
 config.spawnBackMessage = "Change your ped bro -_-"
